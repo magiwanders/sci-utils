276abfeeb60daf4da7def05373d32800fd912ead
magiwanders <simone.shawn.cazzaniga@gmail.com>
2023-07-11 00:48:01 +0200

    new build

--- dist/sci-utils.js
+++ dist/sci-utils.js
@@ -1,9 +1,9 @@
 console.log("SCI Utilities!");
 document.getElementById("calculate").onclick = async () => {
-  let e = p(document.getElementById("metric").value), t = await f(e);
+  let e = m(document.getElementById("metric").value), t = await d(e);
   document.getElementById("results").innerHTML = JSON.stringify(t, null, 4);
 };
-function p(e) {
+function m(e) {
   switch (e) {
     case "gdp":
       return {
@@ -18,6 +18,7 @@ function p(e) {
     case "gdppp":
       return {
         page: "List_of_continents_by_GDP",
+        title: "GDP in US Billions (PPP Adjusted)",
         table: 1,
         fromRow: 2,
         toRow: 7,
@@ -27,51 +28,70 @@ function p(e) {
     case "gdppc":
       return {
         page: "List_of_continents_by_GDP",
-        table: 2
+        title: "GDP Per Capita in US Dollars",
+        table: 2,
+        fromRow: 2,
+        toRow: 7,
+        fromCol: 1,
+        toCol: 2
       };
     case "gdppcpp":
       return {
         page: "List_of_continents_by_GDP",
-        table: 3
+        title: "GDP Per Capita in US Dollars (PPP Adjusted)",
+        table: 3,
+        fromRow: 2,
+        toRow: 7,
+        fromCol: 1,
+        toCol: 2
+      };
+    case "pop":
+      return {
+        page: "List_of_continents_and_continental_subregions_by_population",
+        title: "Population",
+        table: 0,
+        fromRow: 2,
+        toRow: 7,
+        fromCol: 0,
+        toCol: 1
       };
   }
 }
-async function f(e) {
-  let t = "https://www.wikitable2json.com/api/" + e.page + "?table=" + e.table + "&lang=en&cleanRef=false", l = await fetch(t).then((o) => o.json().then((i) => i)), n = c(l[0], e.fromRow, e.toRow, e.fromCol, e.toCol);
-  return u(m(n), e.title);
-}
-function u(e, t) {
-  let l = {
-    title: t
-  };
-  for (let n of e)
-    l[n[0]] = {
-      reale: n[1],
-      persone: n[2]
-    };
-  return l;
-}
-function m(e) {
-  let t = d(e, 1), l = t.reduce((a, s) => a + s, 0), n = document.getElementById("participants").value, o = Math.min(...t), i = t.map((a) => n * (a - o) / (l - o)), r = i.map((a) => Math.floor(a));
-  for (; g(r, n) != 0; )
-    r[_(i, r)] += 1;
-  for (let a in e)
-    e[a].push(r[a]);
-  return e.unshift(["Total", l, n]), e;
-}
-function c(e, t, l, n, o) {
-  return e.slice(t, l + 1).map((i) => i.slice(n, o + 1));
-}
-function d(e, t) {
-  return c(e, 0, e.length, t, t + 1).map((l) => parseFloat(l[0].replace(",", ".")));
+async function d(e) {
+  let t = "https://www.wikitable2json.com/api/" + e.page + "?table=" + e.table + "&lang=en&cleanRef=false", o = await fetch(t).then((i) => i.json().then((a) => a)), l = u(o[0], e.fromRow, e.toRow, e.fromCol, e.toCol);
+  return g(_(l), e.title);
 }
 function g(e, t) {
-  return t - e.reduce((l, n) => l + n, 0);
+  let o = {
+    title: t
+  };
+  for (let l of e)
+    o[l[0]] = {
+      reale: l[1],
+      persone: l[2]
+    };
+  return o;
 }
-function _(e, t) {
-  let l = [];
-  for (let o = 0; o < e.length; o++)
-    l.push(e[o] - t[o]);
-  let n = Math.max(...l);
-  return l.indexOf(n);
+function _(e) {
+  let t = w(e, 1), o = t.reduce((n, c) => n + c, 0), l = document.getElementById("participants").value, i = o / l, a = Math.min(...t), p = t.map((n) => l * (n - a) / (o - a)), r = p.map((n) => Math.round(n));
+  for (; P(r, l) != 0; ) {
+    let n = o, c = 0;
+    for (let s = 0; s < r.length; s++) {
+      let f = Math.abs(p[s] - i * (r[s] + 1));
+      f < n && (n = f, c = s);
+    }
+    r[c] += 1;
+  }
+  for (let n in e)
+    e[n].push(r[n]);
+  return e.unshift(["Total", o, l]), e;
+}
+function u(e, t, o, l, i) {
+  return e.slice(t, o + 1).map((a) => a.slice(l, i + 1));
+}
+function w(e, t) {
+  return u(e, 0, e.length, t, t + 1).map((o) => parseInt(o[0].replaceAll(",", "")));
+}
+function P(e, t) {
+  return t - e.reduce((o, l) => o + l, 0);
 }
